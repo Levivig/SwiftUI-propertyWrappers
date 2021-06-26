7c535f1b6dc1c209432803b07746bddbd0c0b4fd
Levente Vig <levivig@gmail.com>
2021-06-26 09:08:00 +0200

    Initial Commit

---
+++ SwiftUI-propertyWrappers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-propertyWrappers
+//
+//  Created by Levente Vig on 2021. 06. 26..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-propertyWrappers/SwiftUI_propertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_propertyWrappersApp.swift
+//  SwiftUI-propertyWrappers
+//
+//  Created by Levente Vig on 2021. 06. 26..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_propertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
